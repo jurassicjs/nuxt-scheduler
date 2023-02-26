d8e3a929715f4a0b07ffba18ea0db73dd574d3b3
rohrig <richard.t.rohrig@gmail.com>
2023-02-26 01:31:14 +0100

    feature(scheduler) add more time options

--- src/runtime/server/plugins/scheduler.ts
+++ src/runtime/server/plugins/scheduler.ts
@@ -1,8 +1,26 @@
-import startScheduler from '~/server/app/scheduler.ts'
+import startScheduler from '~/server/app/scheduler'
 
-export default defineNitroPlugin(() => {
-  if(startScheduler) {
-    startScheduler()
+const Singleton = (function () {
+  let instance: Object | null;
+
+  function registerSingletonPlugin() {
+      const object = new Object("I am the instance");
+      startScheduler()
+      return object;
   }
 
-})
+  return {
+      getInstance: function () {
+          if (!instance) {
+              instance = registerSingletonPlugin();
+          }
+          return instance;
+      }
+  };
+})();
+
+
+export default function () {
+  Singleton.getInstance()
+}
+
--- src/runtime/server/services/userScheduler.ts
+++ src/runtime/server/services/userScheduler.ts
@@ -1,20 +1,20 @@
 import cron from 'node-cron';
 import consola from 'consola';
 
-export interface Runner {
-  everySecond: () => void;
-  everySeconds: (seconds: number) => void;
-  everyMinute: () => void;
-  everyFiveMinutes: () => void;
-  everyMinutes: (minutes: number) => void;
-  everyHour: () => void;
-  everyHours: (hours: number) => void;
-  everyDay: () => void;
-  everyDays: (days: number) => void;
-  setRawInterval: (interval: string) => void;
-}
+// export interface Runner {
+//   everySecond: () => void;
+//   everySeconds: (seconds: number) => void;
+//   everyMinute: () => void;
+//   everyFiveMinutes: () => void;
+//   everyMinutes: (minutes: number) => void;
+//   everyHour: () => void;
+//   everyHours: (hours: number) => void;
+//   everyDay: () => void;
+//   everyDays: (days: number) => void;
+//   setRawInterval: (interval: string) => void;
+// }
 
-function run(callback: Function): Runner {
+function run(callback: Function) {
   return {
     everySecond: () => {
       cron.schedule('* * * * * *', callback);
@@ -25,11 +25,32 @@ function run(callback: Function): Runner {
     everyMinute: () => {
       cron.schedule('* * * * *', callback);
     },
+    everyTwoMinutes: () => {
+      cron.schedule('*/2 * * * *', callback);
+    },
+    everyThreeMinutes: () => {
+      cron.schedule('*/3 * * * *', callback);
+    },
+    everyFourMinutes: () => {
+      cron.schedule('*/4 * * * *', callback);
+    },
     everyFiveMinutes: () => {
       cron.schedule('*/5 * * * *', callback);
     },
+    everyTenMinutes: () => {
+      cron.schedule('*/10 * * * *', callback);
+    },
+    everyFifteenMinutes: () => {
+      cron.schedule('*/15 * * * *', callback);
+    },
+    everyThirtyMinutes: () => {
+      cron.schedule('*/30 * * * *', callback);
+    },
     everyMinutes: (minutes: number) => {
       cron.schedule(`*/${minutes} * * * *`, callback);
+    },
+    hourly: () => {
+      cron.schedule('0 * * * *', callback);
     },
     everyHour: () => {
       cron.schedule('0 * * * *', callback);
@@ -37,13 +58,43 @@ function run(callback: Function): Runner {
     everyHours: (hours: number) => {
       cron.schedule(`0 */${hours} * * *`, callback);
     },
-    everyDay: () => {
+    hourlyAt: (minute: number) => {
+      cron.schedule(`${minute} * * * *`, callback);
+    },
+    everyOddHour: () => {
+      cron.schedule('0 */2 * * *', callback);
+    },
+    everyTwoHours: () => {
+      cron.schedule('0 */2 * * *', callback);
+    },
+    everyThreeHours: () => {
+      cron.schedule('0 */3 * * *', callback);
+    },
+    everyFourHours: () => {
+      cron.schedule('0 */4 * * *', callback);
+    },
+    everySixHours: () => {
+      cron.schedule('0 */6 * * *', callback);
+    },
+    dailyAt: (hour: number, minute: number) => {
+      cron.schedule(`${minute} ${hour} * * *`, callback);
+    },
+    daily: () => {
       cron.schedule('0 0 * * *', callback);
     },
     everyDays: (days: number) => {
       cron.schedule(`0 0 */${days} * *`, callback);
     },
-    setRawInterval: (interval: string) => {
+    weekly: () => {
+      cron.schedule('0 0 * * 0', callback);
+    },
+    quarterly: () => {
+      cron.schedule('0 0 1 */3 *', callback);
+    },
+    yearly: () => {
+      cron.schedule('0 0 1 1 *', callback);
+    },
+    cron: (interval: string) => {
       cron.schedule(interval, callback);
     }
   };
